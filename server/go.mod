2a0443ca5323db9563a89187ac34bf8ea0ae6b63
Helmut Kemper <helmut.kemper@gmail.com>
2021-07-21 09:34:33 -0300

    initial commit

--- server/go.mod
+++ server/go.mod
@@ -2,5 +2,7 @@ module github.com/helmutkemper/cache.thread.safe.test
 
 go 1.16
 
-//require github.com/helmutkemper/cache.thread.safe v0.5.160
-require github.com/helmutkemper/cache.thread.safe v0.5.165
+require (
+	github.com/brianvoe/gofakeit/v6 v6.5.0
+	github.com/helmutkemper/cache.thread.safe v0.5.190
+)
--- server/main.go
+++ server/main.go
@@ -1,18 +1,220 @@
 package main
 
 import (
-	cache "github.com/helmutkemper/cache.thread.safe/server"
+	"encoding/json"
+	"github.com/brianvoe/gofakeit/v6"
+	cache "github.com/helmutkemper/cache.thread.safe/memory"
 	"log"
 	"os"
 	"sync"
 )
 
+type AuthGoFakeIt struct {
+	Username string
+	Password string
+}
+
+func (e *AuthGoFakeIt) Populate() {
+	e.Username = gofakeit.Username()
+	e.Password = gofakeit.Password(true, true, true, true, true, 20)
+}
+
+type CompanyGoFakeIt struct {
+	BS            string
+	BuzzWord      string
+	Company       string
+	CompanySuffix string
+	Job           *gofakeit.JobInfo
+	JobDescriptor string
+	JobLevel      string
+	JobTitle      string
+}
+
+func (e *CompanyGoFakeIt) Populate() {
+	e.BS = gofakeit.BS()
+	e.BuzzWord = gofakeit.BuzzWord()
+	e.Company = gofakeit.Company()
+	e.CompanySuffix = gofakeit.CompanySuffix()
+	e.Job = gofakeit.Job()
+	e.JobDescriptor = gofakeit.JobDescriptor()
+	e.JobLevel = gofakeit.JobLevel()
+	e.JobTitle = gofakeit.JobTitle()
+}
+
+type PaymentGoFakeIt struct {
+	CreditCard        *gofakeit.CreditCardInfo
+	CreditCardCvv     string
+	CreditCardExp     string
+	CreditCardNumber  string
+	CreditCardType    string
+	Currency          *gofakeit.CurrencyInfo
+	CurrencyLong      string
+	CurrencyShort     string
+	AchRouting        string
+	AchAccount        string
+	BitcoinAddress    string
+	BitcoinPrivateKey string
+}
+
+func (e *PaymentGoFakeIt) Populate() {
+	e.CreditCard = gofakeit.CreditCard()
+	e.CreditCardCvv = gofakeit.CreditCardCvv()
+	e.CreditCardExp = gofakeit.CreditCardExp()
+	e.CreditCardNumber = gofakeit.CreditCardNumber(nil)
+	e.CreditCardType = gofakeit.CreditCardType()
+	e.Currency = gofakeit.Currency()
+	e.CurrencyLong = gofakeit.CurrencyLong()
+	e.CurrencyShort = gofakeit.CurrencyShort()
+	e.AchRouting = gofakeit.AchRouting()
+	e.AchAccount = gofakeit.AchAccount()
+	e.BitcoinAddress = gofakeit.BitcoinAddress()
+	e.BitcoinPrivateKey = gofakeit.BitcoinPrivateKey()
+}
+
+type AddressGoFakeIt struct {
+	Address      *gofakeit.AddressInfo
+	City         string
+	Country      string
+	CountryAbr   string
+	State        string
+	StateAbr     string
+	Street       string
+	StreetName   string
+	StreetNumber string
+	StreetPrefix string
+	StreetSuffix string
+	Zip          string
+	Latitude     float64
+	Longitude    float64
+}
+
+func (e *AddressGoFakeIt) Populate() {
+	e.Address = gofakeit.Address()
+	e.City = gofakeit.City()
+	e.Country = gofakeit.Country()
+	e.CountryAbr = gofakeit.CountryAbr()
+	e.State = gofakeit.State()
+	e.StateAbr = gofakeit.StateAbr()
+	e.Street = gofakeit.Street()
+	e.StreetName = gofakeit.StreetName()
+	e.StreetNumber = gofakeit.StreetNumber()
+	e.StreetPrefix = gofakeit.StreetPrefix()
+	e.StreetSuffix = gofakeit.StreetSuffix()
+	e.Zip = gofakeit.Zip()
+	e.Latitude = gofakeit.Latitude()
+	e.Longitude = gofakeit.Longitude()
+}
+
+type PersonGoFakeIt struct {
+	Id             string
+	Person         *gofakeit.PersonInfo
+	Name           string
+	NamePrefix     string
+	NameSuffix     string
+	FirstName      string
+	LastName       string
+	Gender         string
+	SSN            string
+	Contact        *gofakeit.ContactInfo
+	Email          string
+	Phone          string
+	PhoneFormatted string
+
+	Auth    AuthGoFakeIt
+	Company CompanyGoFakeIt
+	Payment PaymentGoFakeIt
+	Address AddressGoFakeIt
+
+	Bytes int
+}
+
+func (e *PersonGoFakeIt) Populate() (err error) {
+	e.Id = gofakeit.UUID()
+	e.Person = gofakeit.Person()
+	e.Name = gofakeit.Name()
+	e.NamePrefix = gofakeit.NamePrefix()
+	e.NameSuffix = gofakeit.NameSuffix()
+	e.FirstName = gofakeit.FirstName()
+	e.LastName = gofakeit.LastName()
+	e.Gender = gofakeit.Gender()
+	e.SSN = gofakeit.SSN()
+	e.Contact = gofakeit.Contact()
+	e.Email = gofakeit.Email()
+	e.Phone = gofakeit.Phone()
+	e.PhoneFormatted = gofakeit.PhoneFormatted()
+
+	e.Auth.Populate()
+	e.Company.Populate()
+	e.Payment.Populate()
+	e.Address.Populate()
+
+	_, err = json.Marshal(e)
+	return
+}
+
+func (e *PersonGoFakeIt) MarshalJSON() (data []byte, err error) {
+	type personGoFakeIt struct {
+		Id             string
+		Person         *gofakeit.PersonInfo
+		Name           string
+		NamePrefix     string
+		NameSuffix     string
+		FirstName      string
+		LastName       string
+		Gender         string
+		SSN            string
+		Contact        *gofakeit.ContactInfo
+		Email          string
+		Phone          string
+		PhoneFormatted string
+
+		Auth    AuthGoFakeIt
+		Company CompanyGoFakeIt
+		Payment PaymentGoFakeIt
+		Address AddressGoFakeIt
+
+		Bytes int
+	}
+
+	var toJson = personGoFakeIt(*e)
+	data, err = json.Marshal(&toJson)
+	if err != nil {
+		log.Printf("1.PersonGoFakeIt().json.Marshal(&toJson).error: %v", err)
+		return
+	}
+
+	toJson.Bytes = len(data)
+	e.Bytes = toJson.Bytes
+
+	data, err = json.Marshal(&toJson)
+	if err != nil {
+		log.Printf("2.PersonGoFakeIt().json.Marshal(&toJson).error: %v", err)
+		return
+	}
+
+	return
+}
+
 func main() {
 	var err error
 	var wg sync.WaitGroup
 
-	cacheServer := &cache.Server{}
-	err = cacheServer.Init()
+	cacheServer := &cache.Memory{}
+
+	gofakeit.Seed(0)
+
+	log.Printf("populate start")
+	var user = PersonGoFakeIt{}
+	for i := 0; i != 1; i += 800 * 1000 {
+		err = user.Populate()
+		if err != nil {
+			log.Printf("user.Populate().error: %v", err)
+			return
+		}
+	}
+	log.Printf("populate end")
+
+	err = cacheServer.InitServert()
 	if err != nil {
 		log.Printf("error: %v", err.Error())
 	}
@@ -21,54 +223,4 @@ func main() {
 
 	wg.Add(1)
 	wg.Wait()
-}
-
-//func dial() {
-//  time.Sleep(1 * time.Second)
-//  conn, err := grpc.Dial("0.0.0.0:3000", grpc.WithInsecure())
-//  if err != nil {
-//    panic(err)
-//  }
-//  defer conn.Close()
-//
-//  c := cache.NewSyncCacheDaraServerClient(conn)
-//  ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-//  defer cancel()
-//
-//  r, err := c.TakeMyIp(ctx, &cache.TakeMyIpRequest{IP: "127.0.0.1"})
-//  if err != nil {
-//    panic(err)
-//  }
-//
-//  log.Printf("Ok: %v", r.Ok)
-//}
-
-func _main() {
-	var err error
-	//go func() {
-	//  time.Sleep(3*time.Second)
-	//  for i := 0; i != 170; i += 1 {
-	//    go dial()
-	//  }
-	//}()
-
-	//port := cache.KServerPort
-	//lis, err := net.Listen("tcp", port)
-	//if err != nil {
-	//	log.Fatalf("failed to listen: %v", err)
-	//}
-	//s := grpc.NewServer()
-
-	cacheServer := &cache.Server{}
-	err = cacheServer.Init()
-	if err != nil {
-		log.Printf("error: %v", err.Error())
-	}
-
-	log.Printf("starting server: %v", os.Getenv("DEBUG_NAME"))
-
-	//cache.RegisterSyncCacheDaraServerServer(s, cacheServer)
-	//if err := s.Serve(lis); err != nil {
-	//	log.Fatalf("failed to serve: %v", err)
-	//}
 }
